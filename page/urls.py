da9b231f82c03e99fb201235e952f7f215d7fafc
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2023-05-30 18:13:00 +0545

    Image Layout

--- page/urls.py
+++ page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
     path("", index, name="home"),
+    path("test", test, name="test"),
 ]
 
--- page/views.py
+++ page/views.py
@@ -5,6 +5,7 @@ def index(request):
     arts = ArtPiece.objects.all()
     return render(request, 'page/index.html', { 'arts': arts})
 
-
+def test(request):
+    return render(request, 'page/test.html')
 
 
