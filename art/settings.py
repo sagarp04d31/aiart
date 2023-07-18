f32578802ded88dd9f4d12eec664bb258fc9b5ce
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2023-07-18 13:08:56 +0545

    Customized commit message

--- art/settings.py
+++ art/settings.py
@@ -108,7 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 LOGIN_REDIRECT_URL = 'home'
