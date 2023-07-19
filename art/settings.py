520532c70938b0ba2078f3840ef6923827e4153c
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2023-07-19 16:05:08 +0545

    Debug False

--- art/settings.py
+++ art/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#jg4*7eq^&hl#58d8^*1so@ak3-g*75+z#+2+uy_v@@36k^54t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -43,10 +43,11 @@ MIDDLEWARE = [
 ]
 
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-e733.up.railway.app/*',
-    'https://127.0.0.1:8000/',
-]
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = [
+        'https://web-production-e733.up.railway.app/*',
+        'https://127.0.0.1:8000/',
+    ]
 
 
 
